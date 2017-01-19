3b3aa25599c44b403ac2f5e52e70fcbdaa607c8e
craig <waynecraig2008@gmail.com>
2017-01-19 08:18:19 +0800

    improve performance

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const url = require('url');
 
 const express = require('express');
 const app = express();
-const minifyHTML = require('express-minify-html');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const compression = require('compression');
 
 const http = require('http');
 const https = require('https');
@@ -19,26 +17,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser());
 
-app.use('/img', express.static('./img'));
-app.use('/data', express.static('./data'));
+//app.use('/img', express.static('./img'));
+//app.use('/data', express.static('./data'));
 app.use('/cgi-bin/', cgis);
 
-app.use(compression());
-
 app.set('view engine', 'pug')
-
-/*app.use(minifyHTML({
-    override:      true,
-    exception_url: false,
-    htmlMinifier: {
-        removeComments:            true,
-        collapseWhitespace:        true,
-        collapseBooleanAttributes: true,
-        removeAttributeQuotes:     true,
-        removeEmptyAttributes:     true,
-        minifyJS:                  true
-    }
-}));*/
 
 app.get('/list', page);
 app.get('/album/:id', page);
@@ -66,7 +49,7 @@ Promise.all([
     wxapi.init(),
     appjs.init()
 ]).then(() => {
-    https.createServer(config.httpsOpt, app).listen(config.port, () => {
+    http.createServer(app).listen(config.port, () => {
         console.log('listening on port ' + config.port);
     })
 }).catch(e=>console.error(e));
